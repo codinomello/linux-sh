8a49f98b6bdb089987494bc92bdf55f16a596f14
Gabriel <gabrielmellocode@gmail.com>
2025-03-02 19:04:50 -0300

    zsh 🐧

--- config.sh
+++ config.sh
@@ -13,10 +13,10 @@
 [root@PC]$ echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 # (setup dos sudoers)
 
-[root@PC]$ useradd -m -G wheel -s /bin/bash {username}
+[root@PC]$ useradd -m -G wheel -s /bin/bash {usuário}
 # (criação do usuário)
 
-[root@PC]$ passwd {username}
+[root@PC]$ passwd {senha}
 # (senha do usuário)
 
 # --------------------- #
@@ -35,9 +35,54 @@
 [root@PC]$ sudo pacman -Su
 # (atualização do sistema)
 
+[root@PC]$ sudo pacman -S wget
+# (instalação do wget)
+
+# --- #
+# Git #
+# --- #
+
+[root@PC]$ sudo pacman -S git
+# (instalação do git)
+
+[root@PC]$ git --version
+# (verificação da instalação do git [opcional])
+
+[root@PC]$ git config --global user.name {nome}
+# (configuração do nome do usuário)
+
+[root@PC]$ git config --global user.email {e-mail}
+# (configuração do e-mail do usuário)
+
+[root@PC]$ git config --list
+# (verificação das configurações do git [opcional])
+
+[root@PC]$ git config --global core.editor "nvim"
+# (configuração do editor de texto)
+
 # -------- #
 # Neofetch #
 # -------- #
 
 [root@PC]$ sudo pacman -S neofetch
 # (instalação do neofetch)
+
+# ------ #
+# Neovim #
+# ------ #
+
+[root@PC]$ sudo pacman -S neovim
+# (instalação do neovim)
+
+# --------------- #
+# Zsh & Oh My Zsh #
+# --------------- #
+
+[root@PC]$ sudo pacman -S zsh
+# (instalação do zsh)
+
+[root@PC]$ chsh -s $(which zsh)
+# (configuração do shell)
+
+[root@PC]$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# (instalação do oh my zsh)
